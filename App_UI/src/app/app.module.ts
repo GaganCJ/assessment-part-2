98af2a0d759c44d854d3e27fbcba3d06844674a5
GaganCJ <gaganjchandra2379@gmail.com>
2020-03-26 19:14:57 +0530

    Fixing Components #3

--- App_UI/src/app/app.module.ts
+++ App_UI/src/app/app.module.ts
@@ -4,30 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { DataTableModule } from 'angular-datatable';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { TechassessmentComponent } from './register/techassessment/techassessment.component';
-import { BehvassessmentComponent } from './register/behvassessment/behvassessment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminComponent,
     LoginComponent,
-    RegisterComponent,
-    TechassessmentComponent,
-    BehvassessmentComponent
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule,
-    DataTableModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- App_UI/src/app/register/register.component.ts
+++ App_UI/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Assreg } from '../shared/assreg';
+import { RESTAPIService } from '../shared/rest-api-service.service';
 
 @Component({
   selector: 'app-register',
@@ -12,12 +14,33 @@ export class RegisterComponent implements OnInit {
   dd: any = this.today.getDate() + 1;
   mm: any = this.today.getMonth() + 1;
   yyyy: any = this.today.getFullYear();
-  constructor() { }
+  userId: number;
+  TechReg: Assreg;
+  BehvReg: Assreg;
+  constructor(
+    public restApi: RESTAPIService
+  ) { }
 
   ngOnInit() {
     if (this.dd < 10) { this.dd = '0' + this.dd; }
     if (this.mm < 10) { this.mm = '0' + this.mm; }
     this.today = this.yyyy + '-' + this.mm + '-' + this.dd;
+    this.restApi.getUserId().subscribe((data: number) => {
+      this.userId = data;
+    });
+    document.getElementById('dateof').setAttribute('min', this.today);
+  }
+
+  sendTechA() {
+    this.TechReg.userId = this.userId;
+    this.TechReg.assessmentType = 'TECH';
+    this.restApi.postRegDetails(this.TechReg).subscribe((data: any) => {});
+  }
+
+  sendBehvA() {
+    this.BehvReg.userId = this.userId;
+    this.BehvReg.assessmentType = 'BEHV';
+    this.restApi.postRegDetails(this.BehvReg).subscribe((data: any) => {});
   }
 
 }
